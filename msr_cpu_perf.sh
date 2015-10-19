d82ad2b117894038020615c1987c023e4734bb13
nefanov <kolya-efan@mail.ru>
2015-10-20 02:39:58 +0300

    add

--- msr_cpu_perf.sh
+++ msr_cpu_perf.sh
@@ -5,7 +5,8 @@ PercentBusy="\$1"
 #1. Get perf-state:
 MCNT=`rdmsr -p0 IA32_MPERF`
 ACNT=`rdmsr -p0 IA32_APERF`
-
+URC=`rdmsr -p0 IA32_PERF_FIXED_CTR2`
+UCC=`rdmsr -p0 IA32_PERF_FIXED_CTR1`
 PercentPerf=`PercentBusy*(ACNT/MCNT)`
 
 echo 'PercentPerf'
